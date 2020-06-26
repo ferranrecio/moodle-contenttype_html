2f6e8734d242e773fe588f7432b1ec5480e2ec85
Ferran Recio <ferran@moodle.com>
2020-06-26 14:58:15 +0200

    Initial workshop code

---
+++ lang/en/contenttype_html.php
@@ -0,0 +1,35 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for plugin 'contenttype_html'
+ *
+ * @package    contenttype_html
+ * @copyright  2020 Ferran Recio <ferran@moodle.com>
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['cannotloadtemplate'] = 'Unkown template {$a}';
+$string['fullcontent'] = 'Content';
+$string['description'] = 'Basic HTML content';
+$string['templates'] = 'HTML templates';
+$string['template1_name'] = 'Course syllabus';
+$string['pluginname'] = 'HTML';
+$string['pluginname_help'] = 'Create basic HTML content';
+$string['privacy:metadata'] = 'The HTML content bank plugin does not store any personal data.';
+$string['html:access'] = 'Access HTML content in the content bank';
+$string['html:upload'] = 'Upload new HTML content';
+$string['html:useeditor'] = 'Create or edit content using the HTML editor';
